471aa4b67df00913f402ddf07388c65d8ba820e2
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-07-01 18:59:26 -0700

    combine two files that belong to the same repo

--- aws_iam_role.ih-tf-aws-control-289256138624-admin.tf
+++ aws_iam_role.ih-tf-aws-control-289256138624-admin.tf
@@ -1,3 +1,5 @@
+# Roles for CI/CD in the aws-control-289256138624 repo
+
 module "ih-tf-aws-control-289256138624-admin" {
   source = "./modules/tf-admin"
   providers = {
@@ -6,4 +8,15 @@ module "ih-tf-aws-control-289256138624-admin" {
   gh_identity_provider_arn = aws_iam_openid_connect_provider.github.arn
   repo_name                = "aws-control-289256138624"
   state_bucket             = "infrahouse-aws-control-289256138624"
+}
+
+module "ih-tf-aws-control-289256138624-state-manager" {
+  source = "./modules/state-manager-role"
+  providers = {
+    aws = aws.aws-289256138624-uw1
+  }
+  name                      = "ih-tf-aws-control-289256138624-state-manager"
+  gha_role_arn              = module.ih-tf-aws-control-289256138624-admin.gha_role_arn
+  state_bucket              = "infrahouse-aws-control-289256138624"
+  terraform_locks_table_arn = aws_dynamodb_table.terraform_locks.arn
 }
