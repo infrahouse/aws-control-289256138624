82792b6eaad6575ca2b37ab2142e26428ed8f78c
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-07-01 17:06:38 -0700

    Add python requirements

--- data_sources.tf
+++ data_sources.tf
@@ -2,7 +2,3 @@ data "aws_iam_policy" "administrator-access" {
   provider = aws.aws-289256138624-uw1
   name     = "AdministratorAccess"
 }
-
-data "aws_iam_policy_document" "github-aws-control-289256138624" {
-
-}
