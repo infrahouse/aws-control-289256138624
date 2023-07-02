898c687fef94aa9409f271dfe61933bab106b3d7
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2023-07-01 18:18:15 -0700

    Fix typos in state-manager-role module (#4)
    * Fix typos in state-manager-role module
    
    * Add pre-commit hooks

--- aws_iam_role.ih-tf-aws-control-289256138624-state-manager.tf
+++ aws_iam_role.ih-tf-aws-control-289256138624-state-manager.tf
@@ -6,7 +6,7 @@ module "ih-tf-aws-control-289256138624-state-manager" {
     aws = aws.aws-289256138624-uw1
   }
   name                      = "ih-tf-aws-control-289256138624-state-manager"
-  gha_role_arn              = "aws_iam_role.ih-tf-aws-control-289256138624-github.arn"
+  gha_role_arn              = aws_iam_role.ih-tf-aws-control-289256138624-github.arn
   state_bucket              = "infrahouse-aws-control-289256138624"
-  terraform_locks_table_arn = "aws_dynamodb_table.terraform_locks.arn"
+  terraform_locks_table_arn = aws_dynamodb_table.terraform_locks.arn
 }
