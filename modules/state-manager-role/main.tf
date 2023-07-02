18d110bdee523a0d8121aeddf58f939ca1f393e1
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-07-01 17:53:48 -0700

    Add module state-manager-role
    The module creates a role that manages a terraform state for a repo

---
+++ modules/state-manager-role/main.tf
@@ -0,0 +1,65 @@
+## Data Sources
+
+data "aws_iam_policy_document" "assume" {
+  statement {
+    sid     = "000"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::990466748045:user/aleks",
+        var.gha_role_arn
+      ]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "permissions" {
+  statement {
+    actions = ["s3:ListBucket"]
+    resources = [
+      "arn:aws:s3:::${var.state_bucket}"
+    ]
+  }
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject"
+    ]
+    resources = [
+      "arn:aws:s3:::${var.state_bucket}/${var.state_key}"
+    ]
+  }
+  statement {
+    actions = [
+      "dynamodb:DescribeTable",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
+    ]
+    resources = [
+      var.terraform_locks_table_arn
+    ]
+  }
+}
+
+## EOF Data Sources
+
+
+# IAM role
+
+resource "aws_iam_role" "state-manager" {
+  name               = var.name
+  description        = "Role to manage a terraform state of a repo"
+  assume_role_policy = data.aws_iam_policy_document.assume.json
+}
+
+resource "aws_iam_policy" "permissions" {
+  policy   = data.aws_iam_policy_document.permissions.json
+}
+
+resource "aws_iam_role_policy_attachment" "state-manager" {
+  policy_arn = aws_iam_policy.permissions.arn
+  role       = aws_iam_role.state-manager.name
+}
---
+++ modules/state-manager-role/variables.tf
@@ -0,0 +1,22 @@
+variable "gha_role_arn" {
+  description = "Role that assumes a GitHub Actions worker"
+  type = string
+}
+
+variable "name" {
+  description = "Role name"
+}
+
+variable "state_bucket" {
+  description = "Name of the S3 bucket with the state"
+}
+
+variable "state_key" {
+  description = "Path to the state file in the state bucket"
+  type = string
+  default = "terraform.tfstate"
+}
+
+variable "terraform_locks_table_arn" {
+  description = "DynamoDB table that holds Terraform state locks."
+}
