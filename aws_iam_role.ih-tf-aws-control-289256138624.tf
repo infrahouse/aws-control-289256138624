f6815ea8ea5370bb2fc95b2e21f547e7065a593c
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-07-03 16:56:11 -0700

    Configure GitHub OpenID connector using module

--- aws_iam_role.ih-tf-aws-control-289256138624.tf
+++ aws_iam_role.ih-tf-aws-control-289256138624.tf
@@ -5,7 +5,7 @@ module "ih-tf-aws-control-289256138624-admin" {
   providers = {
     aws = aws.aws-289256138624-uw1
   }
-  gh_identity_provider_arn = aws_iam_openid_connect_provider.github.arn
+  gh_identity_provider_arn = module.github-connector.gh_openid_connect_provider_arn
   repo_name                = "aws-control-289256138624"
   state_bucket             = "infrahouse-aws-control-289256138624"
 }
