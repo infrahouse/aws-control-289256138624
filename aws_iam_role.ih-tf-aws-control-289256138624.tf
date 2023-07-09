44948624ec791babcef756fcd33c559d8c5ecc27
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-07-09 08:56:37 -0700

    Organize account_id into a map

--- aws_iam_role.ih-tf-aws-control-289256138624.tf
+++ aws_iam_role.ih-tf-aws-control-289256138624.tf
@@ -6,8 +6,8 @@ module "ih-tf-aws-control-289256138624-admin" {
     aws = aws.aws-289256138624-uw1
   }
   gh_identity_provider_arn = module.github-connector.gh_openid_connect_provider_arn
-  repo_name                = "aws-control-289256138624"
-  state_bucket             = "infrahouse-aws-control-289256138624"
+  repo_name                = "aws-control-${local.aws_account_id.terraform-control}"
+  state_bucket             = "infrahouse-aws-control-${local.aws_account_id.terraform-control}"
 }
 
 module "ih-tf-aws-control-289256138624-state-manager" {
@@ -15,8 +15,8 @@ module "ih-tf-aws-control-289256138624-state-manager" {
   providers = {
     aws = aws.aws-289256138624-uw1
   }
-  name                      = "ih-tf-aws-control-289256138624-state-manager"
+  name                      = "ih-tf-aws-control-${local.aws_account_id.terraform-control}-state-manager"
   gha_role_arn              = module.ih-tf-aws-control-289256138624-admin.gha_role_arn
-  state_bucket              = "infrahouse-aws-control-289256138624"
+  state_bucket              = "infrahouse-aws-control-${local.aws_account_id.terraform-control}"
   terraform_locks_table_arn = aws_dynamodb_table.terraform_locks.arn
 }
