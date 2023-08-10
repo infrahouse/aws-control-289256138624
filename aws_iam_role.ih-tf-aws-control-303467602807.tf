d7dc9bc7815f788272683bad6d2373b1fdc14841
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-08-10 16:34:32 -0700

    Add role ih-tf-aws-control-303467602807-state-manager-read-only
    The role will be used to read from terraform_remote_state

--- aws_iam_role.ih-tf-aws-control-303467602807.tf
+++ aws_iam_role.ih-tf-aws-control-303467602807.tf
@@ -10,3 +10,15 @@ module "ih-tf-aws-control-303467602807-state-manager" {
   state_bucket              = "infrahouse-aws-control-${local.aws_account_id.ci-cd}"
   terraform_locks_table_arn = aws_dynamodb_table.terraform_locks.arn
 }
+
+module "ih-tf-aws-control-303467602807-state-manager-read-only" {
+  source = "./modules/state-manager-role"
+  providers = {
+    aws = aws.aws-289256138624-uw1
+  }
+  name                      = "ih-tf-aws-control-${local.aws_account_id.ci-cd}-state-manager-read-only"
+  assuming_role_arn         = "arn:aws:iam::${local.aws_account_id.management}:role/ih-tf-aws-control-${local.aws_account_id.management}-admin"
+  state_bucket              = "infrahouse-aws-control-${local.aws_account_id.ci-cd}"
+  terraform_locks_table_arn = aws_dynamodb_table.terraform_locks.arn
+  read_only_permissions     = true
+}
