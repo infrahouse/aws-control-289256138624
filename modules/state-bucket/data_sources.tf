1faf810c6bbed5c2189f8c2bb387630a4a243969
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-08-17 10:11:58 -0700

    fix typo - passed object instead of bucket name

--- modules/state-bucket/data_sources.tf
+++ modules/state-bucket/data_sources.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "state" {
   backend = "s3"
   config = {
-    bucket = aws_s3_bucket.state-bucket
+    bucket = aws_s3_bucket.state-bucket.bucket
     key    = "terraform.tfstate"
     region = aws_s3_bucket.state-bucket.region
   }
